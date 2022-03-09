18fcbf31c928ee354ea1a49a3132cbf34a03e54d
simha amara <simhaamara2020@gmail.com>
2022-03-09 21:30:42 +0200

    basic python

---
+++ basic.python.py
@@ -0,0 +1,46 @@
+#1.BASIC
+x=int(input("enter number:"))
+print(x**2)
+
+#2
+x=int(input("enter number:"))
+print(x**2)
+
+
+#3
+x=int(input("enter price:"))
+print(x*1.17)
+
+#4
+x=int(input("enter number:"))
+y=int(input("enter number:"))
+print(x+y,x-y, x*y,x/y )
+
+#5
+print("welcome")
+print("9.3.22")
+print("Wednesday")
+
+#6
+num1=int(input("enter number"))
+num2=int(input("enter number"))
+num3=int(input("enter number"))
+num4=int(input("enter number"))
+print(num1+num2+num3+num4/4)
+
+
+#7
+x=int(input("Nails"))
+y=40
+print(x*1/y)
+
+
+#8
+KM=int(input("enter km"))
+Speed=int(input("enter speed"))
+print(KM/Speed)
+print((KM/Speed)*60)
+
+#10
+n=input("enter number")
+print("the number is: ","0"+n)
